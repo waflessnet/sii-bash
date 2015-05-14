e5382bb8766b2bfe2dc49ba68484c3e3a354c427
Waflessnet <francisco@twpanel.com>
2015-05-14 16:22:31 -0300

    correccion de definicion y agregado controlor de parametros

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # sii-bash
-Autenticacion SII con bash
+### Autenticacion con SII
+Este script bash permite autenticarse frente a los WDSL con el SII de chile mediante el mecanismo de obtencion de semilla, certificacion de esta y obtencion del token.
+#### Requisitos:
+ - Debes poseer un certificado de digital, por lo menos simple, emitido por una entidad certificadora.
+ - Linux
+
+#### Modo de uso
+   
+```sh
+$ git clone https://github.com/waflessnet/sii-bash.git 
+$ cd  sii-bash/
+$ chmod +x consumewsdl.sh
+$ ./consumewsdl.sh </ruta/certificado.pfx> <claveCertificado> <servidor>
+```
+Con esto obtenemos el token de autenticacion del servicio.
+Esta definido por default  produccion: palena.sii.cl, es decir si se omite <servidor>  consume en palena.sii.cl si se agrega maullin consume en maullin.sii.cl.
+Consumo en palena.sii.cl:
+```sh
+$  ./consumewsdl.sh /tmp/certificado.pfx 123456 
+```
+Consumo en maullin.sii.cl:
+```sh
+$  ./consumewsdl.sh /tmp/certificado.pfx 123456  maullin
+```
+Si la clave del certificado utliza caracteres especiales agregamos comillas simples a esta :
+```sh
+$  ./consumewsdl.sh /tmp/certificado.pfx 'wqe@qkj$sdf09' maullin
+```
--- consumewsdl.sh
+++ consumewsdl.sh
@@ -1,26 +1,73 @@
-#!/bin/b
+#!/bin/bash
 # Autor : Francisco Céspedes
 # corremo : ncwapuntes@gmail.com / francisco@twpanel.com
 # 
+# Este script permite conectarse a los WSDL autenticacion del SII y obtener el token de acceso
+# Modo de uso: 
+# chmod +x consumewsdl.sh
+# ./consumewsdl.sh </ruta/certificado.pfx> <clavedelcertificado>
+#
 
-URL_SEMILLA='https://palena.sii.cl/DTEWS/CrSeed.jws'
-URL_TOKEN='https://palena.sii.cl/DTEWS/GetTokenFromSeed.jws'
 SOAPFILE_SEED='rGetSeed.xml'
 SOAPFILE_TOKEN='rGetToken.xml'
 TEMPLATE='template.xml'
 TIMEOUT=15
-CLAVECERTIFICADO='xxxxxxxx'
 SOAPAction_SEMILLA='SOAPAction: "'$URL_SEMILLA'"'
 SOAPAction_TOKEN='SOAPAction: "'$URL_TOKEN'"'
+
+if [ -z $1 ]; then
+  echo "ERROR :-1 Favor ingrese la ruta del certificado"
+  exit;
+  else
+  RUTA_CERTIFICADO=$1
+fi
+
+if [ -z $2 ]; then
+ echo "ERROR: -2 Debe ingresar la clave del certificado"
+ exit;
+ else
+ CLAVECERTIFICADO=$2
+fi
+
+if [ -z $3 ]; then 
+URL_SEMILLA='https://palena.sii.cl/DTEWS/CrSeed.jws'
+URL_TOKEN='https://palena.sii.cl/DTEWS/GetTokenFromSeed.jws'
+else
+SERVER=$3
+ if [ "$SERVER" == "maullin" ]; then
+	URL_SEMILLA='https://maullin.sii.cl/DTEWS/CrSeed.jws'
+	URL_TOKEN='https://maullin.sii.cl/DTEWS/GetTokenFromSeed.jws'
+ else
+	URL_SEMILLA='https://palena.sii.cl/DTEWS/CrSeed.jws'
+	URL_TOKEN='https://palena.sii.cl/DTEWS/GetTokenFromSeed.jws'
+ fi
+fi
+
+if [ ! -f "$RUTA_CERTIFICADO" ]; then
+	echo "Error:1 No existe archivo"
+	exit;
+fi
+
 # extraemos la clave privada 
-openssl pkcs12 -in tmp/certificado.pfx -out tmp/claveprivada.pem -nocerts -nodes -password pass:''$CLAVECERTIFICADO'' > /dev/null 2>&1 
+PKEY=$(openssl pkcs12 -in "$RUTA_CERTIFICADO" -out tmp/claveprivada.pem -nocerts -nodes -password pass:"$CLAVECERTIFICADO" 2>&1)
+if [ "$PKEY" != "MAC verified OK" ]; then
+	echo "Error:2 clave incorrecta del certificado";
+	exit;
+fi
 # extraemos el certificado
-openssl pkcs12 -in tmp/certificado.pfx -out tmp/certificado.pem -nokeys -clcerts -password pass:''$CLAVECERTIFICADO'' > /dev/null 2>&1
+CERT=$(openssl pkcs12 -in "$RUTA_CERTIFICADO" -out tmp/certificado.pem -nokeys -clcerts -password pass:''$CLAVECERTIFICADO''  2>&1)
+if [ "$CERT" != "MAC verified OK" ]; then
+        echo "Error:3 clave incorrecta del certificado";
+        exit;
+fi
 
 #llamadas al servicio de semilla
 curl -s -X POST -H 'Content-Type: text/xml;charset=UTF-8' --data-binary @"${SOAPFILE_SEED}" -H "${SOAPAction_SEMILLA}"  ${URL_SEMILLA} | recode html..ascii > tmp/response-semilla.xml
 SEMILLA=$(cat tmp/response-semilla.xml  | grep -Po '(?<=<SEMILLA>)([^</SEMILLA>]*)')
-
+if [ "$SEMILLA" == "" ]; then
+        echo "Error:4 No existe semilla,favor verifique su acceso al WSDL CrSeed.jws";
+        exit;
+fi
 #generamos el archivo XML en base al template
 sed 's/<Semilla>.*<\/Semilla>/<Semilla>'$SEMILLA'<\/Semilla>/g' template.xml > tmp/semilla.xml
 #Firmar un documento
@@ -34,5 +81,11 @@ sed -i '79s|<\/getToken>|<\/getToken>\]\]><\/pszXml>|' tmp/semilla-firmada.xml
  sed -n "6,8p" rGetToken.xml >> tmp/request-token.xml
 #llamamos el wsdl para obtener el token
 curl -s -X POST -H 'Content-Type: text/xml;charset=UTF-8' --data-binary @tmp/request-token.xml  -H "${SOAPAction_TOKEN}"  ${URL_TOKEN} | recode html..ascii >  tmp/token-xml.xml
+
+ESTADO=$(cat tmp/token-xml.xml | awk -F">" '/ESTADO/{printf $2 }'  | sed 's|</ESTADO||' )
+if [ "$ESTADO" != "00" ]; then
+	echo "Error:5 error al crear el token estado:"$ESTADO
+	exit;
+fi
 #retornamos el token
 cat tmp/token-xml.xml | awk -F">" '/TOKEN/{printf $2 }'  | sed 's|</TOKEN||'
